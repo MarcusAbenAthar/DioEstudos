a79f9859e4c72dba0685cad8ca1b0d709ad03c52
MarcusAbenAthar <marcus.aben@gmail.com>
2022-04-27 08:08:45 -0300

    27/04

--- ReactJS/src/index.js
+++ ReactJS/src/index.js
@@ -1,14 +1,34 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React, { StrictMode } from "react"
+import ReactDOM, { createRoot } from "react-dom/client"
 import "./styles.css"
 
-const App = () => {
+function sum(a, b) {
+    return a + b
+}
+
+function primeiroJSX() {
     return (
-        <div className="App">
-            Hello World
+        <div className="teste">
+            Bruno Carneiro - Introdução ao ReactJS
+            <h4>Soma = {sum(10, 200)}</h4>
         </div>
     )
 }
 
-const rootElement = document.getElementById("root")
-ReactDOM.reader(<App />, rootElement)
+
+const App = () => {
+
+    return (
+        <div className="App">
+            {primeiroJSX()}
+        </div>
+    )
+}
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
