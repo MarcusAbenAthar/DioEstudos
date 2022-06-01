e1c1a5939e514b0a741230bc9ff0a9b47dd9dd07
MarcusAbenAthar <103828180+MarcusAbenAthar@users.noreply.github.com>
2022-06-01 09:53:25 -0300

    js

---
+++ JavaScript/js-modulos/js-import/funcoes.mjs
@@ -0,0 +1,17 @@
+function mostraIdade(nome, idade) {
+    return `${nome} tem ${idade} anos.`;
+}
+
+function mostraCidade(nome, cidade) {
+    return `${nome} mora em ${cidade}.`;
+}
+
+function mostraHobby(nome, hobby) {
+    return `${nome} tem o seguinte hobby: ${hobby}.`;
+}
+
+export {
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+};
---
+++ JavaScript/js-modulos/js-import/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade } from "./funcoes.mjs";
+import { mostraCidade } from "./funcoes.mjs";
+import { mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Marcus', 28));
+console.log(mostraCidade('Marcus', 'Hortolândia'));
+console.log(mostraHobby('Marcus', 'Programar'));
